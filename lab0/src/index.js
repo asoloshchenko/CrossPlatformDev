a7b17d94333a130a9b6079b7089263a1c0f6dfa8
i.stebl <steblivets_i@goldapple.ru>
2024-09-16 20:34:17 +0300

    first commit

---
+++ lab0/src/index.js
@@ -0,0 +1,73 @@
+
+function F1(x) {
+    return x * x - Math.cos(x);
+}
+function F2(x) {
+    return x * x * x + Math.sin(x);
+}
+function F3(x) {
+    return x * x - x + 1;
+}
+
+function LRS(f, a, b){
+    return f(a) * (b-a)
+}
+function LRR(f, a, b){
+    return f(b) * (b-a)
+}
+function MidpointRule(f, a, b){
+    return f((a+b)/2) * (b-a)
+}
+
+const funcs = [F1, F2, F3]
+const methods = [LRS, LRR, MidpointRule]
+function integrate(func, method, a, b,accuracy=0.001, limit = 100) {
+    let prevVal = 0;
+    let curVal = 0;
+    let steps = 2;
+
+    // first iteration
+    let res = 0;
+    let delta = (b - a) / steps;
+
+
+    for (let i = 0; i < steps; i += 1) {
+        res += method(func, a + i*delta, a + (i+1)*delta);
+    }
+    curVal = res;
+
+    steps = steps * 2;
+    i = 0;
+
+    while (Math.abs(prevVal - curVal) > accuracy && i < limit)  {
+
+        res = 0;
+        delta = (b - a) / steps;
+        
+        for (let j = 0; j < steps; j += 1) {
+            res += method(func, a + j*delta, a + (j+1)*delta);
+        }
+        prevVal = curVal;
+        curVal = res;
+
+        steps = steps * 2;
+
+        i+=1;
+
+    }
+
+
+    return res;
+}
+
+let a = parseInt(prompt('Enter a: '));
+let b = parseInt(prompt('Enter b: '));
+
+funcs.forEach(func => {
+    console.log(func.name);
+    methods.forEach(method => {
+        console.log("\t" + method.name);
+        console.log("\t\tres=" + integrate(func, method, a, b))
+    })
+})
+
